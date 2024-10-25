660b5b22832243ebdccdcd969755f9a6f97b0082
Dilshan Hesara <hesarsdilshan@gmail.com>
2024-10-25 18:22:39 +0530

    Employe from Ui Update Con

--- src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/controller/EmployeeController.java
+++ src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/controller/EmployeeController.java
@@ -1,4 +1,115 @@
 package lk.edu.yogurtproduction.yogurtproductionitsolution.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
+
 public class EmployeeController {
+
+    @FXML
+    private TableView<?> EmpTable;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnOpenMailSendModel;
+
+    @FXML
+    private Button btnUpdate;
+
+    @FXML
+    private TableColumn<?, ?> tbEmId;
+
+    @FXML
+    private TableColumn<?, ?> tbMail;
+
+    @FXML
+    private TableColumn<?, ?> tbName;
+
+    @FXML
+    private TableColumn<?, ?> tbNic;
+
+    @FXML
+    private TableColumn<?, ?> tbPhone;
+    @FXML
+    private Button addEmpButt;
+
+    @FXML
+    void btnOpenMailSendModelOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttAddEmp(ActionEvent event) throws IOException {
+
+        try {
+            // Load the mail dialog from FXML file
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/AddEmployee.fxml"));
+            Parent load = loader.load();
+
+         //   SendMailController sendMailController = loader.getController();
+         //   sendMailController.setCustomerEmail(selectedItem.getEmail());
+
+            Stage stage = new Stage();
+            stage.setScene(new Scene(load));
+            stage.setTitle("Add Employee");
+
+            // Set window as modal
+            stage.initModality(Modality.APPLICATION_MODAL);
+
+            stage.initOwner(btnUpdate.getScene().getWindow());
+            stage.showAndWait();
+        } catch (IOException e) {
+            new Alert(Alert.AlertType.ERROR, "Fail to load ui..!");
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void buttDeleteEmp(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttUpadeEmp(ActionEvent event) {
+        try {
+            // Load the mail dialog from FXML file
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/AddEmployee.fxml"));
+            Parent load = loader.load();
+
+            //   SendMailController sendMailController = loader.getController();
+            //   sendMailController.setCustomerEmail(selectedItem.getEmail());
+
+            Stage stage = new Stage();
+            stage.setScene(new Scene(load));
+            stage.setTitle("Add Employee");
+
+            // Set window as modal
+            stage.initModality(Modality.APPLICATION_MODAL);
+
+            stage.initOwner(btnUpdate.getScene().getWindow());
+            stage.showAndWait();
+        } catch (IOException e) {
+            new Alert(Alert.AlertType.ERROR, "Fail to load ui..!");
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void generateAllCustomerReportOnAction(ActionEvent event) {
+
+    }
+
 }
