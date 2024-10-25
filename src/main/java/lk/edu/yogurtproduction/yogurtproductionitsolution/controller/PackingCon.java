65452d1f9fb8ccbdb4c371532ed0751ac525b396
Dilshan Hesara <hesarsdilshan@gmail.com>
2024-10-25 22:03:15 +0530

    Packing From

--- src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/controller/PackingCon.java
+++ src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/controller/PackingCon.java
@@ -1,4 +1,17 @@
 package lk.edu.yogurtproduction.yogurtproductionitsolution.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class PackingCon {
+
+    @FXML
+    private TextField desetxt;
+
+    @FXML
+    void btnPack(ActionEvent event) {
+
+    }
+
 }
