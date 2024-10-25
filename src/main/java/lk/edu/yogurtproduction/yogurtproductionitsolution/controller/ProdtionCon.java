a6d9499f1a8ad3a2ac8c44b3bf8b8f266d5e3a0b
Dilshan Hesara <hesarsdilshan@gmail.com>
2024-10-25 21:34:29 +0530

    Production From

--- src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/controller/ProdtionCon.java
+++ src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/controller/ProdtionCon.java
@@ -1,4 +1,21 @@
 package lk.edu.yogurtproduction.yogurtproductionitsolution.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class ProdtionCon {
+
+    @FXML
+    private Button btnUpdateItem;
+
+    @FXML
+    private TextField txtQty;
+
+    @FXML
+    void btnAddPro(ActionEvent event) {
+
+    }
+
 }
