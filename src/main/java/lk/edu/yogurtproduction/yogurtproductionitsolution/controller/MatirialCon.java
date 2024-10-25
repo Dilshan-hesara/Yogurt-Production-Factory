7f74954d449a388be45ee6f99b9fe711ff8d9e8b
Dilshan Hesara <hesarsdilshan@gmail.com>
2024-10-25 21:17:59 +0530

    Matirial & MatirialUsage From

--- src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/controller/MatirialCon.java
+++ src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/controller/MatirialCon.java
@@ -1,4 +1,47 @@
 package lk.edu.yogurtproduction.yogurtproductionitsolution.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class MatirialCon {
+
+    @FXML
+    private Button btnDeleteItem;
+
+    @FXML
+    private Button btnSaveItem;
+
+    @FXML
+    private Button btnUpdateItem;
+
+    @FXML
+    private Label lblItemId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtPrice;
+
+    @FXML
+    private TextField txtQuantity;
+
+    @FXML
+    void btnDeleteItemOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveItemOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateItemOnAction(ActionEvent event) {
+
+    }
+
 }
