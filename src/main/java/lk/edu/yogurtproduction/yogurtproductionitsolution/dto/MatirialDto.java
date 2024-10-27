370ad7d54a558efe6361a84fbca009c814ee3c2f
Dilshan Hesara <hesarsdilshan@gmail.com>
2024-10-27 21:37:18 +0530

    Matirial Dto & TM

---
+++ src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/dto/MatirialDto.java
@@ -0,0 +1,15 @@
+package lk.edu.yogurtproduction.yogurtproductionitsolution.dto;
+import lombok.*;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class MatirialDto {
+
+    private int matId;
+    private String matName;
+    private String matQty;
+    private String matPrice;
+}
---
+++ src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/dto/TM/MatirialTM.java
@@ -0,0 +1,15 @@
+package lk.edu.yogurtproduction.yogurtproductionitsolution.dto.TM;
+import lombok.*;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class MatirialTM {
+    private int matId;
+    private String matName;
+    private String matQty;
+    private String matPrice;
+
+}
