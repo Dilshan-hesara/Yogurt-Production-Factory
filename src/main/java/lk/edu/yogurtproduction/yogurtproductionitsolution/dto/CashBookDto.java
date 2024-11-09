f42213939aec41ebca1c7fc995abefdf135e34b2
Dilshan Hesara <hesarsdilshan@gmail.com>
2024-11-09 17:04:21 +0530

    CashBook DTO

---
+++ src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/dto/CashBookDto.java
@@ -0,0 +1,15 @@
+package lk.edu.yogurtproduction.yogurtproductionitsolution.dto;
+
+public class CashBookDto {
+
+
+    private String CBNo;
+    private String SupId;
+    private String date;
+    private String desc;
+    private int qty;
+    private double amount;  // Change to double to support decimal amounts
+    private String matID;
+    private String inID;
+
+}
