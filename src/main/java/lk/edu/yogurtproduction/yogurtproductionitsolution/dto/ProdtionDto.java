ec59961cf8701d7d5cf65e79b894f968f4afa878
Dilshan Hesara <hesarsdilshan@gmail.com>
2024-11-10 08:43:01 +0530

    ProdtionDto

---
+++ src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/dto/ProdtionDto.java
@@ -0,0 +1,15 @@
+package lk.edu.yogurtproduction.yogurtproductionitsolution.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class ProdtionDto {
+    private String Prod_ID;
+    private String Pro_Name;
+    private int Prod_Qty;
+}
