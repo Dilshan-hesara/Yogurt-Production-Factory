bb4aed7005c5ccfc39495ebbe9054fefbfc7a98e
Dilshan Hesara <hesarsdilshan@gmail.com>
2024-11-10 08:10:24 +0530

    ProdMix Controller up

--- src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/controller/ProdMixController.java
+++ src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/controller/ProdMixController.java
@@ -7,10 +7,25 @@ import javafx.scene.control.TextField;
 public class ProdMixController {
 
     @FXML
-    private TextField desetxt;
+    private TextField txtJeliy;
+
+    @FXML
+    private TextField txtMilk;
+
+    @FXML
+    private TextField txtProdName;
+
+    @FXML
+    private TextField txtSugur;
+
 
     @FXML
     void btnAddProd(ActionEvent event) {
+        String prodName = txtProdName.getText();
+        int sugur = Integer.parseInt(txtSugur.getText());
+        int jeliy = Integer.parseInt(txtJeliy.getText());
+        int milk = Integer.parseInt(txtMilk.getText());
+
 
     }
 
