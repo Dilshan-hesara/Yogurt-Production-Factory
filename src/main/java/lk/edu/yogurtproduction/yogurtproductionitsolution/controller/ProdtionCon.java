b31eab2805f39a21d515353d8a56adeae6d956d4
Dilshan Hesara <hesarsdilshan@gmail.com>
2024-11-10 08:37:25 +0530

    cmb load ALLprodt names

--- src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/controller/ProdtionCon.java
+++ src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/controller/ProdtionCon.java
@@ -1,14 +1,18 @@
 package lk.edu.yogurtproduction.yogurtproductionitsolution.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import lk.edu.yogurtproduction.yogurtproductionitsolution.model.ProdMixModel;
 import lk.edu.yogurtproduction.yogurtproductionitsolution.model.ProdtionModel;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class ProdtionCon {
 
@@ -16,7 +20,7 @@ public class ProdtionCon {
     private Button btnAddProdt;
 
     @FXML
-    private ComboBox<?> cmbProdt;
+    private ComboBox<String> cmbProdt;
 
     @FXML
     private Label lblProdID;
@@ -24,9 +28,11 @@ public class ProdtionCon {
     @FXML
     private TextField txtQty;
 ProdtionModel model = new ProdtionModel();
+ProdMixModel prodMix = new ProdMixModel();
     public void initialize() throws SQLException {
 
         loadnextProdID();
+        loadProdName();
     }
 
     private void loadnextProdID() throws SQLException {
@@ -36,6 +42,13 @@ ProdtionModel model = new ProdtionModel();
 
 
     }
+    private void loadProdName() throws SQLException {
+        ArrayList<String> prodName = prodMix.getAllProdName();
+        ObservableList<String> observableList = FXCollections.observableArrayList();
+        observableList.addAll(prodName);
+        cmbProdt.setItems(observableList);
+    }
+
 
     @FXML
     void btnAddPro(ActionEvent event) {
--- src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/model/ProdMixModel.java
+++ src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/model/ProdMixModel.java
@@ -3,7 +3,9 @@ package lk.edu.yogurtproduction.yogurtproductionitsolution.model;
 import lk.edu.yogurtproduction.yogurtproductionitsolution.dto.ProdMixDto;
 import lk.edu.yogurtproduction.yogurtproductionitsolution.util.CrudUtil;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class ProdMixModel {
     public boolean saveProdtMaix(ProdMixDto prodMixDto) throws SQLException {
@@ -17,5 +19,18 @@ public class ProdMixModel {
                 prodMixDto.getJeliy()
         );
         return isSaved;
+    }
+
+    public ArrayList<String> getAllProdName() throws SQLException {
+
+        ResultSet rst = CrudUtil.execute("SELECT Prod_Name FROM production_mix_recip");
+
+        ArrayList<String> prodtName = new ArrayList<>();
+
+        while (rst.next()) {
+            prodtName.add(rst.getString(1));
+        }
+
+        return prodtName;
     }
 }
