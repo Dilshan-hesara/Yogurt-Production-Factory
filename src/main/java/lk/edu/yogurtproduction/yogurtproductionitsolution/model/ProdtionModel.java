73953c3428b3ba2df26cafe22f535b9b2d148336
Dilshan Hesara <hesarsdilshan@gmail.com>
2024-11-10 15:09:55 +0530

    Prodtion Tranaction Step 2

--- src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/model/ProdtionModel.java
+++ src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/model/ProdtionModel.java
@@ -1,13 +1,18 @@
 package lk.edu.yogurtproduction.yogurtproductionitsolution.model;
 
+import lk.edu.yogurtproduction.yogurtproductionitsolution.db.DBConnection;
+import lk.edu.yogurtproduction.yogurtproductionitsolution.dto.ProdtionDto;
 import lk.edu.yogurtproduction.yogurtproductionitsolution.util.CrudUtil;
 
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class ProdtionModel {
-    public String getNextProdtID() throws SQLException {
 
+public class ProdtionModel {
+
+    InventroyModel inventoryModel = new InventroyModel();
+    public String getNextProdtID() throws SQLException {
         ResultSet rst = CrudUtil.execute("select Prod_ID from production order by Prod_ID desc limit 1");
         if (rst.next()){
             String lastId = rst.getString(1);
@@ -17,5 +22,37 @@ public class ProdtionModel {
             return String.format("P%03d",newIdIndex);
         }
         return  "P001";
+    }
+
+    public boolean saveProdt(ProdtionDto prodtionDto) throws SQLException {
+
+        Connection connection = DBConnection.getInstance().getConnection();
+        try {
+            connection.setAutoCommit(false);
+
+            boolean isProdtSaved = CrudUtil.execute(
+                    "insert into production values (?,?,?,?)",
+                    prodtionDto.getProd_ID(),
+                    prodtionDto.getPro_Name(),
+                    prodtionDto.getProd_Qty(),
+                    prodtionDto.getProd_Name()
+            );
+            if (isProdtSaved) {
+                System.out.println("dev");
+                boolean isUpdateInverorySaved = inventoryModel.redusqtyOnInventroy(prodtionDto);
+               if (isUpdateInverorySaved) {
+                    connection.commit();
+                    return true;
+                }
+            }
+            connection.rollback();
+            return false;
+        } catch (Exception e) {
+            connection.rollback();
+            return false;
+        } finally {
+            connection.setAutoCommit(true);
+        }
+
     }
 }
