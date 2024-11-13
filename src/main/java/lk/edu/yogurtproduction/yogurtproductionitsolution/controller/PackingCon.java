b332697f7a0e545d7d43d2edade8c3093448d21a
Dilshan Hesara <hesarsdilshan@gmail.com>
2024-11-13 21:42:00 +0530

    Packing Tranaction step 1

--- src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/controller/PackingCon.java
+++ src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/controller/PackingCon.java
@@ -5,16 +5,19 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import lk.edu.yogurtproduction.yogurtproductionitsolution.dto.EmployeeDto;
+import lk.edu.yogurtproduction.yogurtproductionitsolution.dto.PckingDto;
 import lk.edu.yogurtproduction.yogurtproductionitsolution.dto.ProdtionDto;
 import lk.edu.yogurtproduction.yogurtproductionitsolution.model.EmployeeModel;
+import lk.edu.yogurtproduction.yogurtproductionitsolution.model.PackingModel;
 import lk.edu.yogurtproduction.yogurtproductionitsolution.model.ProdtionModel;
 
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class PackingCon {
 
@@ -30,6 +33,7 @@ public class PackingCon {
     @FXML
     private TextField qrytxt;
 
+
     @FXML
     private Label lblEmpName;
 
@@ -39,22 +43,67 @@ public class PackingCon {
     @FXML
     private Label lblProdtName;
 
+    @FXML
+    private DatePicker expireDate;
+
+    @FXML
+    private ComboBox<String> cmbPacType;
+
 
     public  void initialize() throws SQLException {
         loadProdtId();
         loadEmpId();
-
+        loadcmbPacType();
     }
-
+PackingModel packingModel = new PackingModel();
     @FXML
-    void btnAddStock(ActionEvent event) {
+    void btnAddStock(ActionEvent event) throws SQLException {
+
+         String Pac_ID = "PAC002";
+         String Prod_ID = cmbProdId.getSelectionModel().getSelectedItem();
+         String Pac_Type = cmbPacType.getSelectionModel().getSelectedItem();
+         String Pac_Desc = desetxt.getText();
+         String Pac_Date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+         String Expire_Date = String.valueOf(expireDate.getValue());
+         double Qty = Double.parseDouble(qrytxt.getText());
+         String Emp_ID = cmbEmpId.getSelectionModel().getSelectedItem();
+         double RedusQty = Qty * PacTypes;
+         String itemType = "END Prodt";
+
+        PckingDto pckingDtos = new PckingDto(
+
+                Pac_ID,
+                Prod_ID,
+                Pac_Type,
+                Pac_Desc,
+                Pac_Date,
+                Expire_Date,
+                Qty,
+                Emp_ID,
+                RedusQty,
+                itemType
+        );
+        boolean isSaved = packingModel.savePacking(pckingDtos);
+        if (isSaved) {
+            new Alert(Alert.AlertType.INFORMATION, "Saved successfully!").show();
+        } else {
+            new Alert(Alert.AlertType.ERROR, "Save failed! Please try again.").show();
+        }
+
+
+
 
     }
 
     @FXML
     void btnPack(ActionEvent event) {
+        System.out.println(PacTypes);
+        String cmbPacTypeSelected = cmbPacType.getSelectionModel().getSelectedItem();
+        System.out.println(cmbPacTypeSelected);
+        System.out.println( expireDate.getValue());
 
     }
+
 
 
     @FXML
@@ -66,6 +115,7 @@ public class PackingCon {
         }
 
     }
+
 
     @FXML
     void cmbProdOnAction(ActionEvent event) throws SQLException {
@@ -94,5 +144,34 @@ public class PackingCon {
         ObservableList<String> observableList = FXCollections.observableArrayList(prodIds);
         cmbProdId.setItems(observableList);
     }
+
+    int PacTypes = 0;
+
+    @FXML
+    void CmbPacTypeOnAction(ActionEvent event) {
+
+
+        String cmbPacTypeSelected = cmbPacType.getSelectionModel().getSelectedItem();
+        if (cmbPacTypeSelected != null) {
+
+            if (cmbPacTypeSelected == "08 PACK SET") {
+                System.out.println(cmbPacTypeSelected);
+                PacTypes = 8;
+            } else if (cmbPacTypeSelected == "12 PACK SET") {
+                System.out.println(cmbPacTypeSelected);
+                PacTypes = 12;
+
+            } else if (cmbPacTypeSelected == "48 PACK SET") {
+                System.out.println(cmbPacTypeSelected);
+                PacTypes = 48;
+            }
+        }
+
+    }
+    private void loadcmbPacType() {
+        cmbPacType.setItems(FXCollections.observableArrayList("08 PACK SET","12 PACK SET", "48 PACK SET"));
+    }
+
+
 
 }
