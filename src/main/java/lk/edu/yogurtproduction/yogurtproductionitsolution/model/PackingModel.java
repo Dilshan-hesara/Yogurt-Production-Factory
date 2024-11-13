793eb99ec3d4c3dabac39794456ec7af4ff6d6ae
Dilshan Hesara <hesarsdilshan@gmail.com>
2024-11-13 21:42:19 +0530

    Packing Tranaction step 2

--- src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/model/PackingModel.java
+++ src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/model/PackingModel.java
@@ -1,4 +1,47 @@
 package lk.edu.yogurtproduction.yogurtproductionitsolution.model;
 
+import lk.edu.yogurtproduction.yogurtproductionitsolution.db.DBConnection;
+import lk.edu.yogurtproduction.yogurtproductionitsolution.dto.PckingDto;
+import lk.edu.yogurtproduction.yogurtproductionitsolution.util.CrudUtil;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class PackingModel {
+    public boolean savePacking(PckingDto pckingDtos) throws SQLException {
+        Connection connection = DBConnection.getInstance().getConnection();
+        try {
+            connection.setAutoCommit(false);
+
+            boolean isRecpSaved = CrudUtil.execute(
+                    "INSERT INTO packing VALUES (?, ?, ?, ?, ?, ?, ?,?)",
+                    pckingDtos.getPac_ID(),
+                    pckingDtos.getProd_ID(),
+                    pckingDtos.getPac_Type(),
+                    pckingDtos.getPac_Desc(),
+                    pckingDtos.getPac_Date(),
+                    pckingDtos.getExpire_Date(),
+                    pckingDtos.getQty(),
+                    pckingDtos.getEmp_ID()
+            );
+
+            if (isRecpSaved) {
+                System.out.println("savesd");
+                connection.commit();
+                return true;
+
+
+            }
+
+            connection.rollback();
+            return false;
+        } catch(SQLException e){
+            connection.rollback();
+            e.printStackTrace();
+            return false;
+        } finally{
+            connection.setAutoCommit(true);
+        }
+
+    }
 }
