3ecab9b757f7fbb922e6a267f077a7728da5aed2
Dilshan Hesara <hesarsdilshan@gmail.com>
2024-11-13 18:40:56 +0530

    Packing Dto

--- src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/dto/PckingDto.java
+++ src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/dto/PckingDto.java
@@ -1,4 +1,21 @@
 package lk.edu.yogurtproduction.yogurtproductionitsolution.dto;
+import lombok.*;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 
 public class PckingDto {
+
+    private String Pac_ID;
+    private String Prod_ID;
+    private String Packing_Type;
+    private String Packing_Description;
+    private String Packing_Date;
+    private String Expire_Date;
+    private double qty;
+    private  String Emp_ID;
+
+
 }
