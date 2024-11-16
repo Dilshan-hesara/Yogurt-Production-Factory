f08348bb2255f7c3c442d1bfd535d3293ee012c6
Dilshan Hesara <hesarsdilshan@gmail.com>
2024-11-16 07:14:07 +0530

    CashBook TranAction Step 01

--- src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/dto/CashBookDto.java
+++ src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/dto/CashBookDto.java
@@ -17,26 +17,12 @@ public class CashBookDto {
 
     private String CBNo;
     private String SupId;
-    private String date;
+    private String matID;
+    private String inID;
     private String desc;
     private int qty;
     private double amount;
-    private String matID;
-    private String inID;
-    private String itemType;
-    private String Prod_id;
+    private String date;
 
 
-    public CashBookDto(String string, String string1, String string2, String string3, int anInt, double aDouble) {
-        this.CBNo = string;
-        this.SupId = string1;
-        this.date = string2;
-        this.desc = string3;
-        this.qty = anInt;
-        this.amount = aDouble;
-        this.matID = string3;
-        this.inID = string;
-        this.itemType = string;
-
-    }
 }
