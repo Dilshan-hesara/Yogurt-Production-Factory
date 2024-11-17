039a2308f24219517eaaefc108e4e3b3d902c203
Dilshan Hesara <hesarsdilshan@gmail.com>
2024-11-17 20:11:44 +0530

    RestPassword From laod

---
+++ src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/controller/RestPassWordController.java
@@ -0,0 +1,30 @@
+package lk.edu.yogurtproduction.yogurtproductionitsolution.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class RestPassWordController {
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private TextField txtNewPassWord;
+
+    @FXML
+    private TextField txtOldPassWord;
+
+    @FXML
+    private TextField txtRenterNewPassWord;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void btnSaveRestPasss(ActionEvent event) {
+
+    }
+
+}
