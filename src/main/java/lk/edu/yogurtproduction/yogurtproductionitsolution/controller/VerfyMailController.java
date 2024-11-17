e35a01e3dcc5696b0de9fae575dd470cbfd3cc18
Dilshan Hesara <hesarsdilshan@gmail.com>
2024-11-17 22:46:17 +0530

    Creat VerfyMail

---
+++ src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/controller/VerfyMailController.java
@@ -0,0 +1,28 @@
+package lk.edu.yogurtproduction.yogurtproductionitsolution.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class VerfyMailController  {
+
+    @FXML
+    private Button Acc;
+
+    @FXML
+    private Label txtMail;
+
+    @FXML
+    private Label txtName;
+
+    @FXML
+    private TextField txtOtp;
+
+    @FXML
+    void btnSaveAcc(ActionEvent event) {
+
+    }
+
+}
