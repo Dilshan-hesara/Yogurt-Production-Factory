a23ddbdf321b8613d7ff09c1718bc99c72a7d104
Dilshan Hesara <hesarsdilshan@gmail.com>
2024-11-18 14:11:16 +0530

    RestPassWord Form up

--- src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/controller/RestPassWordController.java
+++ src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/controller/RestPassWordController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import lk.edu.yogurtproduction.yogurtproductionitsolution.model.UserModel;
 
 import java.sql.SQLException;
@@ -25,6 +26,10 @@ public class RestPassWordController {
 
     @FXML
     private TextField txtUserName;
+
+    @FXML
+    private AnchorPane nextPage;
+
 
     UserModel userModel = new UserModel();
 
