5a84196adf974ed2c0b339d9139f61f41c1ff816
Dilshan Hesara <hesarsdilshan@gmail.com>
2024-11-21 19:51:03 +0530

    Orders TranAction Step 4

--- src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/model/OrderDetailsModel.java
+++ src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/model/OrderDetailsModel.java
@@ -1,11 +1,13 @@
 package lk.edu.yogurtproduction.yogurtproductionitsolution.model;
 
 import lk.edu.yogurtproduction.yogurtproductionitsolution.dto.OrderDetailsDto;
+import lk.edu.yogurtproduction.yogurtproductionitsolution.util.CrudUtil;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class OrderDetailsModel {
-    public boolean saveOrderDetailsList(ArrayList<OrderDetailsDto> orderDetailsDTOS){
+    public boolean saveOrderDetailsList(ArrayList<OrderDetailsDto> orderDetailsDTOS) throws SQLException {
 
         for (OrderDetailsDto orderDetailsDTO : orderDetailsDTOS) {
             boolean isOrderDetailsSaved = saveOrderDetail(orderDetailsDTO);
@@ -14,13 +16,19 @@ public class OrderDetailsModel {
             }
 
 
+
         }
         return true;
     }
 
-    private boolean saveOrderDetail(OrderDetailsDto orderDetailsDTO) {
+    private boolean saveOrderDetail(OrderDetailsDto orderDetailsDTO) throws SQLException {
 
-
-        return false;
+        return CrudUtil.execute(
+                "insert into orderdetails values (?,?,?,?)",
+                orderDetailsDTO.getOrderId(),
+                orderDetailsDTO.getItemId(),
+                orderDetailsDTO.getQuantity(),
+                orderDetailsDTO.getPrice()
+        );
     }
 }
