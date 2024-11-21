809cb5a8d7770f629b73da276cf1143f92466dee
Dilshan Hesara <hesarsdilshan@gmail.com>
2024-11-21 11:38:39 +0530

    User Details Verfy Step 1

--- src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/controller/UserDetailsController.java
+++ src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/controller/UserDetailsController.java
@@ -3,14 +3,24 @@ package lk.edu.yogurtproduction.yogurtproductionitsolution.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import lk.edu.yogurtproduction.yogurtproductionitsolution.util.UserName;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class UserDetailsController {
+public class UserDetailsController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        userName = UserName.getUsername();
+        lblUserNama.setText(userName);
+
+    }
 
     @FXML
     private Button btnChangePass;
@@ -21,24 +31,29 @@ public class UserDetailsController {
     @FXML
     private AnchorPane nextPage;
 
-    @FXML
-    void btnChangePass(ActionEvent event) throws IOException {
-        System.out.println(userName);
-//        nextPage.getChildren().clear();
-//
-//        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/UserDetailsNewPassW.fxml"));
-//        AnchorPane load = loader.load();
-//
-//
-//        Stage stage = (Stage) nextPage.getScene().getWindow();
-//        stage.setTitle("Reset Password");
-//
-//        nextPage.getChildren().add(load);
-    }
-
     String userName;
     public void sendUserName(String userName) {
-    this.userName = userName;
-    lblUserNama.setText(userName);
+        this.userName = userName;
+        lblUserNama.setText(userName);
     }
+
+    @FXML
+    void btnChangePass(ActionEvent event) throws IOException {
+
+        nextPage.getChildren().clear();
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/UserDetailsNewPassW.fxml"));
+        AnchorPane load = loader.load();
+
+
+        UserDetailsNewPassW pasUserName = loader.getController();
+        pasUserName.setUserDetails(userName);
+
+        Stage stage = (Stage) nextPage.getScene().getWindow();
+        stage.setTitle("Reset Password");
+
+        nextPage.getChildren().add(load);
+    }
+
+
 }
